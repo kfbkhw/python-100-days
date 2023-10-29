0d6a84d403537f1141f31f18411c4b7b11310de5
Hailey Kim <134510274+kfbkhw@users.noreply.github.com>
2023-10-29 17:46:22 +0900

    Update main.py via upload

--- day52-instagram-follower-bot/main.py
+++ day52-instagram-follower-bot/main.py
@@ -1,23 +1,4 @@
-from selenium import webdriver
-
-FOLLOW_ACCOUNT = "bestvacations"
-USERNAME = "haileyprogramming"
-PASSWORD = "haileycoding"
-
-
-class InstaFollower:
-    def __init__(self):
-        self.driver = webdriver.Chrome(options=webdriver.ChromeOptions().add_experimental_option("detach", True))
-
-    def login(self):
-        pass
-
-    def find_followers(self):
-        pass
-
-    def follow(self):
-        pass
-
+from instafollower import InstaFollower
 
 bot = InstaFollower()
 bot.login()
