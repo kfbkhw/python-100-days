a08bcc8e8616b082351a2e0f347ef88729b8c559
Hailey Kim <134510274+kfbkhw@users.noreply.github.com>
2023-11-01 19:53:51 +0900

    Update file via upload

--- day54-web-development-with-flask/hello.py
+++ day54-web-development-with-flask/hello.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/bye")
+def good_bye():
+    return "<p>Bye!</p>"
+
+
+if __name__ == "__main__":
+    app.run()
