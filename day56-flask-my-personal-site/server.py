213675557eac8401cc7cf0a90db02ed712090138
Hailey Kim <134510274+kfbkhw@users.noreply.github.com>
2023-11-03 16:55:46 +0900

    Update server.py via upload

--- day56-flask-my-personal-site/server.py
+++ day56-flask-my-personal-site/server.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def rendering_html_static_files():
+    return render_template("cv/index.html")
+
+
+@app.route("/0")
+def rendering_html_css():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
