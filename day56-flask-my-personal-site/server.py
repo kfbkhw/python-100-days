0c208b6fd20a2d95fcf46743bc1157fb168e5884
Hailey Kim <134510274+kfbkhw@users.noreply.github.com>
2023-11-04 12:21:05 +0900

    Update server.py via upload

--- day56-flask-my-personal-site/server.py
+++ day56-flask-my-personal-site/server.py
@@ -18,5 +18,20 @@ def render_personal_site():
     return render_template("personal-site/index.html")
 
 
+@app.route("/index")
+def render_downloaded_site_index():
+    return render_template("html5up-massively/index.html")
+
+
+@app.route("/generic")
+def render_downloaded_site_generic():
+    return render_template("html5up-massively/generic.html")
+
+
+@app.route("/elements")
+def render_downloaded_site_elements():
+    return render_template("html5up-massively/elements.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
